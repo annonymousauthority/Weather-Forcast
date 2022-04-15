483b3ec618987bef6f48e022dfc43ab204ad2ccc
khite-engineering <72549011+khite-engineering@users.noreply.github.com>
2022-04-15 15:36:34 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -61,13 +61,13 @@ exports.addLocation = functions.https.onRequest(async (req, res) => {
               "max":maxTemp
             }
             const writeResult = admin.firestore().collection("location").doc(original).collection(todayDate).doc(original).set({"date": todayDate, "min": minTemp, "max": maxTemp, location: original }, {merge: true})
-           
+            
           })
         })
         _request.on('error', error => {
           console.error(error)
         })
-        _request.end()
+       
         })
         
       })
@@ -79,12 +79,12 @@ exports.addLocation = functions.https.onRequest(async (req, res) => {
       var query = {};
       var todayDate = new Date().toISOString().slice(0, 10);
       var forcastRef = admin.firestore().collection("location").doc(original).collection(todayDate).doc(original)
-      const forcast = await forcastRef.get().then(doc => {
+      const forcast = await forcastRef.get().then(function(doc) {
         if (doc.empty) {
           console.log("It's empty")
         }else{
           query = doc.data()
-          // console.log(query)
+          console.log("query", query)
         }
       })
       .catch(err => {
@@ -97,7 +97,7 @@ exports.addLocation = functions.https.onRequest(async (req, res) => {
           "min-Forcasted": query.min,
           "max-Forcasted":query.max
         }
-        // console.log(query)
+        console.log(query)
       }else{
        var resultJson = "Try again"
       }
@@ -107,83 +107,7 @@ exports.addLocation = functions.https.onRequest(async (req, res) => {
     res.json({result: resultJson});
 });
 
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-// exports.updateForcast = functions.firestore.document('/{location}/{documentId}/{dateCollection}/{documentId}')
-//     .onCreate((snap, context) => {
-//         var data = [];
-//         var sdata = "";
-//         var latitude = "";
-// 	      var longitude = "";
-//         var locationdata = [];
-//         var jsonLocationdata = "";
-//         var temp = [];
-//         var maxTemp = [];
-//         var minTemp = [];
 
-//         // var json = "";
-        
-//         const original = snap.data();
-//         const json ={
-//           "date" : snap.data().date,
-//           "min-Forcasted": snap.data().min,
-//           "max-Forcasted": snap.data().max
-//         }
-//         const options = "https://maps.googleapis.com/maps/api/geocode/json?address=" + original + ",+CA&key=" + "AIzaSyDBTFOUT_88qqdmi4cqV31LDpX-2YAqaYY";
-//         // const req = https.request(options, res => {
-//         //     console.log(`statusCode: ${res.statusCode}`)
-          
-//         //     res.on('data',function(chunk){
-//         //         data.push(chunk)
-//         //     })
-//         //     res.on('end', function(){
-//         //         sdata = JSON.parse(data.join(''))
-//         //         latitude = sdata.results[0].geometry.location.lat
-//         //         longitude = sdata.results[0].geometry.location.lng
-//         //         console.log(longitude)
-//         //         const forcastoptions = "https://www.7timer.info/bin/astro.php?lon=" + longitude + "&lat=" + latitude + "&ac=0&unit=metric&output=json&tzshift=0"
-//         //     const req = https.request(forcastoptions, res => {
-//         //       console.log(`statusCode: ${res.statusCode}`)
-            
-//         //       res.on('data',function(chunk){
-//         //         locationdata.push(chunk) 
-//         //       })
-      
-//         //       res.on('end',function(){
-//         //         jsonLocationdata = JSON.parse(locationdata.join(''))
-//         //         for (let index = 0; index < jsonLocationdata.dataseries.length; index++) {
-//         //           temp.push(jsonLocationdata.dataseries[index].temp2m);
-//         //         }
-//         //         console.log(temp)
-//         //         minTemp = Math.min.apply(null, temp),
-//         //         maxTemp = Math.max.apply(null, temp);
-//         //         var todayDate = new Date().toISOString().slice(0, 10);
-//         //         console.log(todayDate)
-//         //         console.log(minTemp,maxTemp)
-//         //         json = {
-//         //           "date" : todayDate,
-//         //           "min-forecasted":minTemp,
-//         //           "max-forecasted":maxTemp
-//         //         }
-//         //         // snap.ref.set({"date": todayDate, "min-forcasted": minTemp, "max-forcasted": maxTemp }, {merge: true})
-               
-//         //       })
-//         //     })
-//         //     req.on('error', error => {
-//         //       console.error(error)
-//         //     })
-//         //     req.end()
-//         //     })
-            
-//         //   })
-//         //   req.on('error', error => {
-//         //     console.error(error)
-//         //   })
-//         // req.end()
-
-        
-//       return console.log(json)
-// });
 
 function checkForcast (lat, long) {
     var locationdata = [];
@@ -236,27 +160,30 @@ exports.searchForForcast = functions.https.onRequest(async(req, res) =>{
   for(var arr=[],dt=new Date(start_date); dt<=new Date(end_date); dt.setDate(dt.getDate()+1)){
     dateRange.push(new Date(dt).toISOString().slice(0, 10));
   }
-  console.log(dateRange[0].toString());
+  console.log(dateRange);
   if (dateRange.length == 1) {
-    var forcastRef =  admin.firestore().collection("location/"+location+"/"+dateRange[0].toString()).doc(location)
+    var forcastRef =  admin.firestore().collection("location/"+location+"/"+dateRange[0]).doc(location)
     const forcast = await forcastRef.get().then(doc => {
       query.push(doc.data())
+      console.log(doc.data())
     })
     .catch(err => {
       console.log('Error getting document', err);
     });
   }else if (dateRange.length > 1) {
     for (let index = 0; index < dateRange.length; index++) {
-      var forcastRef = await admin.firestore().collection("location"+location+dateRange[index].toString()).doc(location).get().then(function(snapshot) {
+      var forcastRef = await admin.firestore().collection("location/"+location+"/"+dateRange[index].toString()).doc(location).get().then(function(snapshot) {
           if (snapshot.empty) {
             console.log("snapshot is empty")
           }else{
             query.push(snapshot.data())
+            console.log(query)
           }
       })
     }
   }else{
     console.log("input the right date range")
   }
+  
   res.json({result: query})
 })
